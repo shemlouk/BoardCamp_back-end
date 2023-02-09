43cac09b75804226ef12aaac6514168b7836eed3
samuel <shemlouk@gmail.com>
2023-02-09 15:34:07 -0300

    refactor(games): add BASE_URL const to endpoints

--- src/routers/Games.ts
+++ src/routers/Games.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 
+const BASE_URL = "/games";
 const route = Router();
 
-route.get("/games");
-route.post("/games");
+route.get(BASE_URL);
+route.post(BASE_URL);
 
 export default route;
