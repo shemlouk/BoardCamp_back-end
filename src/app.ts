1a22687fc981c96a8a480191649b6651481f4b7c
samuel <shemlouk@gmail.com>
2023-02-09 15:37:14 -0300

    feat: add routers to app server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import Games from "./routers/Games";
+import Rentals from "./routers/Rentals";
+import Customers from "./routers/Customers";
 
 dotenv.config();
 
@@ -8,6 +11,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(Games);
+app.use(Rentals);
+app.use(Customers);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
