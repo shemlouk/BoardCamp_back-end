f5c3bc96639d48fef2b4b1c619ead14a9a03d928
samuel <shemlouk@gmail.com>
2023-02-12 15:24:09 -0300

    feat: add read and create functions from controller to endpoints

--- src/routers/Games.ts
+++ src/routers/Games.ts
@@ -1,9 +1,10 @@
+import { read, create } from "../controllers/gamesController";
 import { Router } from "express";
 
 const BASE_URL = "/games";
 const route = Router();
 
-route.get(BASE_URL);
-route.post(BASE_URL);
+route.get(BASE_URL, read);
+route.post(BASE_URL, create);
 
 export default route;
