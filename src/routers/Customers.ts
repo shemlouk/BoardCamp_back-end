14de405db8dd6530fce8fbc196a5b7d672f64598
samuel <shemlouk@gmail.com>
2023-02-12 18:35:08 -0300

    feat: add read, readById, create and update functions from controller to endpoints

--- src/routers/Customers.ts
+++ src/routers/Customers.ts
@@ -1,11 +1,12 @@
+import * as C from "../controllers/customersController";
 import { Router } from "express";
 
 const BASE_URL = "/customers";
 const router = Router();
 
-router.get(BASE_URL);
-router.get(BASE_URL + "/:id");
-router.put(BASE_URL);
-router.post(BASE_URL);
+router.get(BASE_URL, C.read);
+router.post(BASE_URL, C.create);
+router.put(BASE_URL + "/:id", C.update);
+router.get(BASE_URL + "/:id", C.readById);
 
 export default router;
