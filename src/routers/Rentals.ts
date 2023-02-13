9056adf0d5106367a3fadd23a89e1d3ea1042c35
samuel <shemlouk@gmail.com>
2023-02-13 11:28:07 -0300

    feat: add read, create, returnOne and deleteOne functions from controller to endpoints

--- src/routers/Rentals.ts
+++ src/routers/Rentals.ts
@@ -1,11 +1,12 @@
+import * as C from "../controllers/rentalsController";
 import { Router } from "express";
 
 const BASE_URL = "/rentals";
 const router = Router();
 
-router.get(BASE_URL);
-router.post(BASE_URL);
-router.post(BASE_URL + "/:id/return");
-router.delete(BASE_URL + "/:id");
+router.get(BASE_URL, C.read);
+router.post(BASE_URL, C.create);
+router.post(BASE_URL + "/:id/return", C.returnOne);
+router.delete(BASE_URL + "/:id", C.deleteOne);
 
 export default router;
